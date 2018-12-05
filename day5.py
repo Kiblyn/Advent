3e09fa1778e9047ffa05dde3e75618f36f551fc8
Kiblyn <45605761+Kiblyn@users.noreply.github.com>
2018-12-05 19:22:21 +0100

    - sale et + opti

--- day5.py
+++ day5.py
@@ -1,15 +1,12 @@
 def full_react(tab):
-    different = True
-    while different:
-        i = 0
-        different = False
-        while i < len(tab) - 1:
-            if tab[i] != tab[i+1] and tab[i].lower() == tab[i+1].lower():
-                tab.pop(i)
-                tab.pop(i)
-                different = True
-            else:
-                i += 1
+    i = 0
+    while i < len(tab) - 1:
+        if tab[i] != tab[i+1] and tab[i].lower() == tab[i+1].lower():
+            tab.pop(i)
+            tab.pop(i)
+            i -= 1
+        else:
+            i += 1
     return len(tab)
 
 tab = [c for c in open("input5.txt").read()]
