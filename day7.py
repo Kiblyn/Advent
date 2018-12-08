1312b60973c58177618f2e02f8834c4112b26f01
Kiblyn <45605761+Kiblyn@users.noreply.github.com>
2018-12-08 15:15:19 +0100

    Partie 2

--- day7.py
+++ day7.py
@@ -1,7 +1,7 @@
 lignes = open("input.txt").read().splitlines()
 lettres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 tab = list((ligne[5], ligne[36]) for ligne in lignes)
-requirements = {x:[y[0] for y in tab if y[1]==x] for x in lettres}
+requis = {x:[y[0] for y in tab if y[1]==x] for x in lettres}
 resultat = ""
 while len(resultat) < 26:
     for c in lettres:
@@ -11,3 +11,22 @@ while len(resultat) < 26:
             break
 
 print("Partie 1:", resultat)
+
+temps_travail = [1]*5
+unite_travail = ["" for _ in range(5)]
+utilises = set()
+finis = set()
+compteur = 0
+while temps_travail != [0]*5 :
+    temps_travail = [max(i-1,0) for i in temps_travail]
+    for i, temps in enumerate(temps_travail):
+        if temps == 0:
+            finis.add(unite_travail[i])
+    disponibles = sorted(x for x in lettres if x not in utilises and all(y in finis for y in requis[x]))
+    for i, temps in enumerate(temps_travail):
+        if temps == 0 and len(disponibles) > 0:
+            unite_travail[i] = disponibles[0]
+            temps_travail[i] = ord(disponibles[0])-4
+            utilises.add(disponibles.pop(0))
+    compteur += 1
+print("Partie 2:", compteur - 1)
