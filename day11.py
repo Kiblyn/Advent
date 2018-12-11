804f8950d7e218c22ea44444fcb5f88d3a4b8784
Kiblyn <45605761+Kiblyn@users.noreply.github.com>
2018-12-11 23:47:28 +0100

    Opti stockage somme rectangulaire
    Stockage des sommes des rectangles pour pouvoir calculer l'intégralité des sommes possibles

--- day11.py
+++ day11.py
@@ -1,22 +1,19 @@
-sommes = []
-for y in range(300):
+sommes = [[0]*301]
+for y in range(1,301):
     sommes.append([0])
-    for x in range(300):
-        sommes[y].append(sommes[y][-1]+(((x+10)*y+9110)*(x+10)//100)%10-5)
-
+    for x in range(1,301):
+        sommes[y].append(sommes[y-1][x]+sommes[y][x-1]-sommes[y-1][x-1]+(((x+10)*y+9110)*(x+10)//100)%10-5)
 maxi = 0
 nmax = 0
-for n in range(3,60):
-    for y in range(301-n):
-        for x in range(301-n):
-            s = sum(sommes[y][x+n]-sommes[y][x] for y in range(y, y+n))
+for n in range(3,301):
+    for y in range(1,301-n):
+        for x in range(1,301-n):
+            s = sommes[y-1][x-1]+sommes[y+n-1][x+n-1]-sommes[y+n-1][x-1]-sommes[y-1][x+n-1]
             if s > maxi:
                 maxi = s
                 pos = (x,y)
                 nmax = n
     if n == 3:
         res = pos
-    if n>nmax+5:
-        break
 print("Partie 1:", res)
 print("Partie 2:", pos, nmax)
